f2c40dfcafde6cae7bf15a0cfb79bddc0eced170
Dhafir Tsabit <dhafirtsaabit@gmail.com>
2025-10-01 15:00:31 +0700

    mengubah sout kedua

--- Minggu6/iam.java
+++ Minggu6/iam.java
@@ -2,6 +2,6 @@ public class iam {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-        System.out.println("Rafael kampret");
+        System.out.println("Hi!");
     }
 }
