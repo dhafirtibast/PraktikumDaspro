2dc7825874b8020454de8b682769d4a9865becac
Dhafir Tsabit <dhafirtsaabit@gmail.com>
2025-10-07 15:42:35 +0700

    Tugas 2

---
+++ Minggu7/jobsheet5/ganjilGenap.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ganjilGenap {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int angka = 0;
+        System.out.print("Masukkan angka: ");
+        angka = sc.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Genap");
+        } else {
+            System.out.println("Ganjil");
+        }
+    }
+}
