0f4c35ff36daa71983e0ab2ce9205cc398ca4d6f
Dhafir Tsabit <dhafirtsaabit@gmail.com>
2025-10-07 13:13:55 +0700

    Modifikasi percobaan 1

--- Minggu7/jobsheet5/ifCetakKRS11.java
+++ Minggu7/jobsheet5/ifCetakKRS11.java
@@ -12,6 +12,8 @@ public class ifCetakKRS11 {
             System.out.println("Pembayaran UKT terverifikasi.");
             System.out.println("Silakan cetak KRS dan minta tanda tangan DPA.");
 
+        }else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
         }
 
     }
