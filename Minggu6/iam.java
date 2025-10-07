ce7188cdf6c0b8e53b9d4940dfff93445ad4f305
dhafirtibast <dhafirtsaabit@gmail.com>
2025-10-07 15:52:06 +0700

    Update iam.java

--- Minggu6/iam.java
+++ Minggu6/iam.java
@@ -2,6 +2,6 @@ public class iam {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-        System.out.println("Hi!");
+        System.out.println("World: "Hi!" ");
     }
 }
