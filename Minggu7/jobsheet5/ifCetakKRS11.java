3aa4cf5c18afdb06b49d4a2358cb5292ec3458a4
Dhafir Tsabit <dhafirtsaabit@gmail.com>
2025-10-08 14:22:30 +0700

    Tugas 1

--- Minggu7/jobsheet5/ifCetakKRS11.java
+++ Minggu7/jobsheet5/ifCetakKRS11.java
@@ -9,16 +9,8 @@ public class ifCetakKRS11 {
         boolean uktLunas = input.nextBoolean();
         System.out.println(" ");
 
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT terverifikasi.");
-            System.out.println("Silakan cetak KRS dan minta tanda tangan DPA.");
-
-        } else {
-            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-        }
-
-        //String pesan = (uktLunas) ? "Pembayaran UKT terverifikasi. Silakan cetak KRS dan minta tanda tangan DPA." : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
-        //System.out.println(pesan);
-        //System.out.println(" ");
+        String pesan = (uktLunas) ? "Pembayaran UKT terverifikasi. Silakan cetak KRS dan minta tanda tangan DPA." : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
+        System.out.println(pesan);
+        System.out.println(" ");
     }
 }
