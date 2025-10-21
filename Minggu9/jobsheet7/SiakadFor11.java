bc8e367f3e1d502622ae8fe136e10ad7037a4e02
Dhafir Tsabit <dhafirtsaabit@gmail.com>
2025-10-21 12:46:01 +0700

    Percobaan 1

--- Minggu9/jobsheet7/SiakadFor11.java
+++ Minggu9/jobsheet7/SiakadFor11.java
@@ -3,7 +3,22 @@ public class SiakadFor11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        
+        double nilai, terendah = 100, tertinggi = 0;
+
+        for (int i=1; i<=10; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-"+ i +": ");
+            nilai = sc.nextDouble();
+            if (nilai > tertinggi) {
+                tertinggi = nilai;
+            }
+            if (nilai < terendah) {
+                terendah = nilai;
+            }
+        }
+        System.out.println("Nilai tertinggi: " + tertinggi);
+        System.out.println("Nilai terendah: " + terendah);
+
     }
+
     
 }
