d239033f272cff74a7c4ac994a9b2424d9c78212
Dhafir Tsabit <dhafirtsaabit@gmail.com>
2025-10-25 15:59:18 +0700

    Modifikasi percobaan 2

--- Minggu9/jobsheet7/SiakadWhile11.java
+++ Minggu9/jobsheet7/SiakadWhile11.java
@@ -4,14 +4,15 @@ public class SiakadWhile11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int nilai, jml, i = 0;
+        int jml, i = 0;
+        double nilai;
 
         System.out.print("Masukkan jumlah mahasiswa: ");
         jml = sc.nextInt();
 
         while (i < jml) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + ": ");
-            nilai = sc.nextInt();
+            nilai = sc.nextDouble();
 
             if (nilai < 0 || nilai > 100) {
                 System.out.println("Nilai tidak valid. Masukkan lagi nilai yang valid!");
@@ -20,6 +21,7 @@ public class SiakadWhile11 {
 
             if (nilai > 80 && nilai <= 100) {
                 System.out.println("Nilai mahasiswa ke-" + (i+1) + " adalah A");
+                System.out.println("Bagus, pertahankan nilainya!");
             } else if (nilai > 73 && nilai <= 80) {
                 System.out.println("Nilai mahasiswa ke-" + (i+1) + " adalah B+");
             } else if (nilai > 65 && nilai <= 73) {
