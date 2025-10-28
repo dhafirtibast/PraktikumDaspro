4524eebfc966014b83f14dfd75262514f6100038
Dhafir Tsabit <dhafirtsaabit@gmail.com>
2025-10-28 23:43:13 +0700

    Modifikasi percobaan 4

--- Minggu10/Jobsheet8/NilaiKelompok11.java
+++ Minggu10/Jobsheet8/NilaiKelompok11.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class NilaiKelompok11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
+        float rataTertinggi = 0;
+        int nomorKelompokTop = 0;
         int i = 1;
-        while (i <= 6) {
+        while (i <= 2) {
             System.out.println("Kelompok " + i);
  
             float totalNilai = 0;
@@ -17,7 +19,14 @@ public class NilaiKelompok11 {
             float rataNilai = totalNilai/5;
             System.out.print("Kelompok " + i + ": ");
             System.out.println("nilai rata-rata = " + rataNilai);
+
+            if (rataNilai > rataTertinggi) {
+                rataTertinggi = rataNilai;
+                nomorKelompokTop = i;
+            }
             i++;
         }
+        System.out.print("Kelompok dengan rata-rata tertinggi adalah Kelompok ");
+        System.out.println(nomorKelompokTop + " dengan rata-rata " + rataTertinggi);
     }
 }
