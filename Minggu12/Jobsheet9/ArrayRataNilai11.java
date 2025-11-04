55dbf0979eecd5442178f8c53c09bf77d4587e53
Dhafir Tsabit <dhafirtsaabit@gmail.com>
2025-11-04 15:02:41 +0700

    Modifikasi percobaan 3

--- Minggu12/Jobsheet9/ArrayRataNilai11.java
+++ Minggu12/Jobsheet9/ArrayRataNilai11.java
@@ -3,10 +3,15 @@ public class ArrayRataNilai11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] nilaiMhs = new int[10];
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jmlMhsw = sc.nextInt();
+        int[] nilaiMhs = new int[jmlMhsw];
         int jmlMhsLulus = 0;
-        double total = 0;
-        double rata2;
+        int jmlMhsTdkLulus = 0;
+        double totalLulus = 0;
+        double totalTdkLulus = 0;
+        double rata2MhsLulus;
+        double rata2MhsTdkLulus;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
@@ -15,12 +20,16 @@ public class ArrayRataNilai11 {
         for (int i = 0; i < nilaiMhs.length; i++) {
             if (nilaiMhs[i] > 70) {
                 jmlMhsLulus ++;
+                totalLulus += nilaiMhs[i];
+            } else {
+                jmlMhsTdkLulus++;
+                totalTdkLulus += nilaiMhs[i];
             }
-            total += nilaiMhs[i];
         }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = "+rata2);
-        System.out.println("Jumlah mahasiswa lulus: "+jmlMhsLulus);
+        rata2MhsLulus = totalLulus/jmlMhsLulus;
+        rata2MhsTdkLulus = totalTdkLulus/jmlMhsTdkLulus;
+        System.out.println("Rata-rata nilai lulus = "+ rata2MhsLulus);
+        System.out.println("Rata-rata nilai tidak lulus = "+ rata2MhsTdkLulus);
 
     }
 }
