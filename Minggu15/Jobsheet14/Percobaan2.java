cc191b526fcdcc34cf227fbb923cdd06e5a76482
Dhafir Tsabit <dhafirtsaabit@gmail.com>
2025-12-02 15:02:16 +0700

    update percobaan 2

--- Minggu15/Jobsheet14/Percobaan2.java
+++ Minggu15/Jobsheet14/Percobaan2.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class Percobaan2 {
     static int hitungPangkat(int a, int b) {
         if (b == 0) {
+            System.out.print("1");
             return (1);
         } else {
+            System.out.print(a + "x");
             return (a * hitungPangkat(a, b - 1));
         }
     }
@@ -16,6 +18,9 @@ public class Percobaan2 {
         bilangan = sc.nextInt();
         System.out.print("Pangkat: ");
         pangkat = sc.nextInt();
-        System.out.println(hitungPangkat(bilangan, pangkat));
+        
+        // menampilkan hasil
+        int hasil = hitungPangkat(bilangan, pangkat);
+        System.out.print(" = " + hasil);
     }
 }
